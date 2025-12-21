53db02bd1537e99aa439e3e9de9e04ed3a521c43
Tat1209 <77540698+Tat1209@users.noreply.github.com>
2025-12-21 17:05:13 +0900

    sync: from A6000

--- ee/ee_experiments/20251210_epoch/main.py
+++ ee/ee_experiments/20251210_epoch/main.py
@@ -1,24 +1,26 @@
 from exp import exp
-from gpu_scheduler import parallel_run
+from gpu_scheduler import parallel_run, generate_tasks_grid
 
 def main():
     cfg = {}
 
-    cfg["exp_name"] = "exp_bl"
+    cfg["exp_name"] = "exp_epoch_re"
     cfg["train_ds_str"] = "cifar100_train"
     cfg["val_ds_str"] = "cifar100_val"
 
-    cfg["wd"] = [0.5, 5e-4, 0]
-    cfg[("optim_str", "max_lr")] = list(zip(["adamw", "sgd"], [0.005, 0.1]))
+    cfg["wd"] = [5e-1, 5e-2, 5e-4, 0]
+    cfg["optim_str"] = "adamw"
+    cfg["max_lr"] = 0.005
 
     model_str = ["resnet18"]
     batch_size = [128]
     cfg[("model_str", "batch_size")] = list(zip(model_str, batch_size))
 
 
-    ndata = [50000, 5000, 500, 100] * 2
+    ndata = [50000, 5000, 500, 100]
     epochs = [200 if n >= 10000 else 10000 * 200 // n for n in ndata] + [200 if n >= 20000 else 20000 * 200 // n for n in ndata]
-    cfg[("ndata", "epochs")] = list(zip(ndata, epochs))
+    base_epochs = [10000] * 4 + [20000] * 4
+    cfg[("ndata", "epochs", "base_epochs")] = list(zip(ndata * 2, epochs, base_epochs))
 
     cfg["div"] = [1, 16]
     
@@ -28,7 +30,14 @@ def main():
     cfg["num_workers"] = 2
     cfg["compile"] = True
     
-    parallel_run(task_func=exp, config=cfg, avoid_used=True, gpu_ids=[0,1,2,3])
+    tasks = generate_tasks_grid(exp, cfg)
+    parallel_run(tasks, avoid_used=True, gpu_ids=[0,1,2,3,4,5,6,7])
+
+    cfg["wd"] = [5e-2, 5e-3, 5e-4, 0]
+    cfg["optim_str"] = "sgd"
+    cfg["max_lr"] = 0.1
+    tasks = generate_tasks_grid(exp, cfg)
+    parallel_run(tasks, avoid_used=True, gpu_ids=[0,1,2,3,4,5,6,7])
 
 if __name__ == "__main__":
     main()
