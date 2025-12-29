f8c8f795df5896ca8ed8d6235f41592422911146
Tat1209 <taikunn12740pqp@icloud.com>
2025-12-29 21:57:11 +0900

    sync: from wsl

--- ee/ee_experiments/20251218_analyze/train.py
+++ ee/ee_experiments/20251218_analyze/train.py
@@ -21,10 +21,11 @@ from exp_manager import ExpManager
 from torchvision import models
 from trainer import MultiTrainer, Trainer
 
+
 def exp(cfg: dict):
     torch.set_num_threads(cfg["num_threads"]) 
     torch.set_num_interop_threads(cfg["num_interop_threads"])
-    src_text, src_name = utils.get_source(path=this_path, with_name=True)
+    src_text, src_name= utils.get_source(path=this_path, with_name=True)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -51,6 +52,9 @@ def exp(cfg: dict):
     base_val_ds = fetch_handler(ds_root, val_ds_str)
     
     match train_ds_str:
+        # case "cifar10_train":
+            # train_trans = [transforms.ToImage(), transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip(), transforms.RandomRotation(15), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
+            # val_trans = [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
         case "cifar100_train":
             train_trans = [transforms.ToImage(), transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip(), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
             val_trans = [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
@@ -58,9 +62,7 @@ def exp(cfg: dict):
             train_trans = [transforms.ToImage(), transforms.RandomHorizontalFlip(p=0.5), transforms.RandomRotation(degrees=(0, 360)), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
             val_trans = [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True), base_train_ds.normalizer()]
         case _:
-            # default transforms or raise error
-            train_trans = []
-            val_trans = []
+            pass
 
     train_ds = base_train_ds.transform(train_trans).balance_class(seed=0).in_ndata(ndata)
     val_ds = base_val_ds.transform(val_trans)
@@ -69,51 +71,49 @@ def exp(cfg: dict):
     val_dl = val_ds.loader(batch_size, shuffle=False, num_workers=cfg["num_workers"])
 
     exp = ExpManager(exp_path=this_path.parent / exp_name, exp_tpl="exp_tpl_ee")
-    run = exp.create_run()
+    run_mgr = exp.create_run()
 
-    run.log_param("model_arc", f"{net.__name__}")
-    
-    run.log_param("train_dataset", train_ds.state.dataset_id)
-    run.log_param("val_dataset", val_ds.state.dataset_id)
-    run.log_param("num_classes", num_classes := train_ds.fetch_classes())
+    run_mgr.log_param("model_arc", f"{net.__name__}")
+    # run_mgr.log_param("model_arc", f"{net.__module__} {net.__name__}")
 
-    run.log_param("train_trans", repr(train_trans))
-    run.log_param("val_trans", repr(val_trans))
+    run_mgr.log_param("train_dataset", train_ds.state.dataset_id)
+    run_mgr.log_param("val_dataset", val_ds.state.dataset_id)
+    run_mgr.log_param("num_classes", num_classes := train_ds.fetch_classes())
 
-    run.log_param("train_ndata", len(train_ds))
-    run.log_param("val_ndata", len(val_ds))
+    run_mgr.log_param("train_trans", repr(train_trans))
+    run_mgr.log_param("val_trans", repr(val_trans))
 
-    run.log_param("epochs", epochs)
-    run.log_param("max_lr", max_lr)
-    run.log_param("wd", wd)
-    run.log_param("batch_size", batch_size)
+    run_mgr.log_param("train_ndata", len(train_ds))
+    run_mgr.log_param("val_ndata", len(val_ds))
 
-    run.log_param("iters/epoch", len(train_dl))
-    run.log_param("iters", len(train_dl) * epochs)
-    run.log_param("processed_ndata", ndata * epochs)
-    run.log_param("ndata_per_class", len(train_ds) / num_classes)
-    run.log_param("div", div)
-    run.log_param("ensembles", ens)
-    run.log_param("scale_ch", scale_ch)
-    run.log_text(src_name, src_text)
+    run_mgr.log_param("epochs", epochs)
+    run_mgr.log_param("max_lr", max_lr)
+    run_mgr.log_param("wd", wd)
+    run_mgr.log_param("batch_size", batch_size)
+
+    run_mgr.log_param("base_epochs", cfg.get("base_epochs", None))
+    run_mgr.log_param("iters/epoch", len(train_dl))
+    run_mgr.log_param("iters", len(train_dl) * epochs)
+    run_mgr.log_param("processed_ndata", ndata * epochs)
+    run_mgr.log_param("ndata_per_class", len(train_ds) / num_classes)
+    run_mgr.log_param("div", div)
+    run_mgr.log_param("ensembles", ens)
+    run_mgr.log_param("scale_ch", scale_ch)
+    run_mgr.log_text(src_name, src_text)
 
     if cfg.get("cifar_style"):
         network = Network(EERefiner(net(num_classes=num_classes)).cifar_style().multi_narrow(div=div, agg="mean", flex_ch=cfg["flex_ch"]).init_weights().build().to(device))
     else:
         network = Network(EERefiner(net(num_classes=num_classes)).multi_narrow(div=div, agg="mean", flex_ch=cfg["flex_ch"]).init_weights().build().to(device))
-    
     if cfg["compile"]:
         network = torch.compile(network, mode="max-autotune")
-    
     criterion = torch.nn.CrossEntropyLoss()
-    
     if optim_str == "sgd":
         optimizer = torch.optim.SGD(network.parameters(), lr=max_lr, momentum=0.9, weight_decay=wd, nesterov=True, fused=True)
     elif optim_str == "adam":
         optimizer = torch.optim.Adam(network.parameters(), lr=max_lr, weight_decay=wd, fused=True)
     elif optim_str == "adamw":
         optimizer = torch.optim.AdamW(network.parameters(), lr=max_lr, weight_decay=wd, fused=True)
-    
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0, last_epoch=-1)
     trainer = Trainer(network, criterion, optimizer, scheduler, device, dtype=torch.bfloat16)
             
@@ -124,11 +124,10 @@ def exp(cfg: dict):
         "scheduler": trainer.fmt_scheduler(),
     }
 
-    run.log_params(hp_dict)
-    run.log_text("model_repr.txt", network.repr_network())
-    run.log_text("model_torchinfo.txt", network.torchinfo(dl=train_dl))
+    run_mgr.log_params(hp_dict)
+    run_mgr.log_text("model_repr.txt", network.repr_network())
+    run_mgr.log_text("model_torchinfo.txt", network.torchinfo(dl=train_dl))
 
-    # Training Loop
     for e in range(epochs):
         lr = trainer.get_lr()
 
@@ -139,16 +138,22 @@ def exp(cfg: dict):
             val_loss, val_acc = trainer.val_1epoch(val_dl)
             mets |= {"val_loss": val_loss, "val_acc": val_acc}
         else:
+            # val_aux = {}
             mets |= {"val_loss": None, "val_acc": None}
 
-        run.log_metrics(mets, step=e + 1)
-        run.log_metrics(trainer.time_stats(incl_fmt=True), step=e + 1)
-        run.log_metric("progress", f"{(e + 1) / epochs * 100:.1f}%", step=e + 1)
-        run.sync(step=e + 1, itv=epochs/100, last_step=epochs)
+        run_mgr.log_metrics(mets, step=e + 1)
+        # run_mgr.log_metrics(train_aux, step=e + 1)
+        # run_mgr.log_metrics(val_aux, step=e + 1)
+        # run_mgr.log_metrics(trainer.time_info(), step=e + 1)
+        run_mgr.log_metrics(trainer.time_stats(incl_fmt=True), step=e + 1)
 
+        run_mgr.log_metric("progress", f"{(e + 1) / epochs * 100:.1f}%", step=e + 1)
+        run_mgr.sync(step=e + 1, itv=epochs/100, last_step=epochs)
         trainer.printmet(mets, e + 1, epochs, itv=epochs / 5)
 
-        torch.save(trainer.network.state_dict(), run.fpath("state_dict.pt"))
-        exp.ref_results()
+    torch.save(trainer.network.state_dict(), run_mgr.fpath("state_dict.pt"))
+    exp.ref_results() # 競合しないように，書き込みは一回だけ
+    
+    print(f"Experiment completed. Results saved to {run_mgr.run_path}")
 
-        print(f"Experiment completed. Results saved to {run.run_path}")
+
