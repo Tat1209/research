455b0bcee5cb41c6cf5e41a78d8b57b60d5c790b
Tat1209 <taikunn12740pqp@icloud.com>
2026-02-18 17:47:50 +0900

    sync: from wsl

--- ee/ee_experiments/20251213_baseline_gr/main_test.py
+++ ee/ee_experiments/20251213_baseline_gr/main_test.py
@@ -10,22 +10,24 @@ def main():
     # cfg["exp_name"] = "exp_tmp"
     cfg["exp_name"] = "exp_model_bl_best"
     cfg["train_ds_str"] = "cifar100_train"
-    cfg["val_ds_str"] = "cifar100_val"
-    cfg["optim_str"] = "SGD"
-    # cfg["optim_str"] = "adamw"
+    cfg["val_ds_str"] = "cifar100_test"
+    # cfg["optim_str"] = "SGD"
+    cfg["optim_str"] = "adamw"
 
     model_str = ["resnet18"]
     batch_size = [128]
-    max_lr = [1e-3, 3e-3, 5e-3, 1e-2, 3e-2, 5e-2, 1e-1, 3e-1, 5e-1]
+    # max_lr = [1e-3, 3e-3, 5e-3, 1e-2, 3e-2, 5e-2, 1e-1, 3e-1, 5e-1]
 
+    max_lr = [1e-3, 3e-3, 5e-3]
     # max_lr = [1e-5, 3e-5, 1e-4, 3e-4, 5e-4, 1e-3, 3e-3, 5e-3]
     # max_lr = [base_lr * batch_size / 128 for batch_size in batch_size]
     # cfg[("model_str", "batch_size", "max_lr")] = list(zip(model_str, batch_size, max_lr))
     cfg["max_lr"] = max_lr
     cfg[("model_str", "batch_size")] = list(zip(model_str, batch_size))
 
+    cfg["wd"] = [1e-2, 3e-2, 5e-2, 7e-2]
     # cfg["wd"] = [1e-1, 3e-1, 5e-1, 7e-1, 1, 3, 5, 7, 10, 12, 15, 18, 20, 25, 30]
-    cfg["wd"] = [1e-4, 5e-4, 1e-3, 3e-3, 5e-3, 1e-2, 3e-2, 5e-2, 7e-2, 1e-1, 3e-1, 5e-1, 7e-1, 1]
+    # cfg["wd"] = [1e-4, 5e-4, 1e-3, 3e-3, 5e-3, 1e-2, 3e-2, 5e-2, 7e-2, 1e-1, 3e-1, 5e-1, 7e-1, 1]
 
     ndata = [1000]
 
@@ -44,15 +46,15 @@ def main():
     
     tasks = generate_tasks_grid(exp, cfg)
 
-    key_map = {
-        "model_str": "model_arc",    # run内で net.__name__ としてログ保存
-        "ndata": "train_ndata",      # run内で len(train_ds) としてログ保存
-        "wd": "wd",                # そのままログ保存
-        "epochs": "epochs",          # そのままログ保存（ndataと連動するが念のため）
-        "optim_str": "optim_str",
-        "batch_size": "batch_size"   # そのままログ保存（model_strと連動するが念のため）
-    }
-    tasks = filter_finished_tasks(tasks, parquet_path=f"{this_path.parent}/{cfg['exp_name']}/_results.parquet", key_map=key_map)
+    # key_map = {
+    #     "model_str": "model_arc",    # run内で net.__name__ としてログ保存
+    #     "ndata": "train_ndata",      # run内で len(train_ds) としてログ保存
+    #     "wd": "wd",                # そのままログ保存
+    #     "epochs": "epochs",          # そのままログ保存（ndataと連動するが念のため）
+    #     "optim_str": "optim_str",
+    #     "batch_size": "batch_size"   # そのままログ保存（model_strと連動するが念のため）
+    # }
+    # tasks = filter_finished_tasks(tasks, parquet_path=f"{this_path.parent}/{cfg['exp_name']}/_results.parquet", key_map=key_map)
 
     parallel_run(tasks, avoid_used=True, gpu_ids=[0,1,2,3,4,5,6,7])
 
